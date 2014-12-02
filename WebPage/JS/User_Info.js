f797350633dcf3a8d501feb0bdbaf953500d8a4a
KajDime <dkajhanidis@hotmail.com>
2014-12-02 23:03:55 +0100

    Bootstrap Test

--- WebPage/JS/User_Info.js
+++ WebPage/JS/User_Info.js
@@ -1,7 +1,7 @@
 var UserInfo = angular.module('UserInfo',[]);
 UserInfo.controller('Login',function($scope)
 {
-	$scope.setInfo = {Name:'Name', PWord:'PassWord'};
+	$scope.setInfo = {Name:'User Name', PWord:'PassWord'};
 	$scope.Submit_c = function()
 	{
 		//send ifo to server!!!
