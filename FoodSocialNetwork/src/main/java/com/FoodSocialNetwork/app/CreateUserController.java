324e0c796023d02418458960595613560e5d963d
Padde <patrik.carl.eriksson@gmail.com>
2014-12-02 15:45:21 +0100

    Create Account in Database

--- FoodSocialNetwork/src/main/java/com/FoodSocialNetwork/app/CreateUserController.java
+++ FoodSocialNetwork/src/main/java/com/FoodSocialNetwork/app/CreateUserController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.FoodSocialNetwork.app.database.DatabaseHandler;
 import com.FoodSocialNetwork.app.responce.CreateAccountResponce;
 
 @RestController
@@ -11,11 +12,7 @@ public class CreateUserController {
 	
     @RequestMapping("/createAccount")
     public CreateAccountResponce greeting(@RequestParam(value="username") String name,@RequestParam(value="password") String password, @RequestParam(value="email") String email,@RequestParam(value="country") String country) {
-    	
-    	CreateAccountResponce returnValue = new CreateAccountResponce();
-    	
-    	returnValue.setSuccess(true);
-    	
-    	return returnValue;
+    	    	
+    	return new DatabaseHandler().createAccount(name, password, email, country);
     }
 }
--- FoodSocialNetwork/src/main/java/com/FoodSocialNetwork/app/database/DatabaseHandler.java
+++ FoodSocialNetwork/src/main/java/com/FoodSocialNetwork/app/database/DatabaseHandler.java
@@ -6,6 +6,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import com.FoodSocialNetwork.app.responce.CreateAccountResponce;
+
 
 public class DatabaseHandler {
 	
@@ -34,54 +36,32 @@ public class DatabaseHandler {
 		}
 	}
 	
+	public CreateAccountResponce createAccount(String userName,String password,String email,String country)
+	{
+		String sql = "Insert into user(username,password,email,country) value(?,?,?,?)";
+		CreateAccountResponce CAR = new CreateAccountResponce();
+		try
+		{
+			PreparedStatement ps = connection.prepareStatement(sql);
+			ps.setString(1, userName);
+			ps.setString(2, password);
+			ps.setString(3, email);
+			ps.setString(4, country);
+			
+			ps.execute();
+			CAR.setSuccess(true);
+		}
+		catch(Exception e)
+		{
+			CAR.setSuccess(false);
+			CAR.setError(e.getMessage());
+		}
+		
+		return CAR;
+	}
 	
-	
-//	  public static void main(String[] argv) throws SQLException {
-//	 
-//		System.out.println("-------- MySQL JDBC Connection Testing ------------");
-//	 
-//		try {
-//			Class.forName("com.mysql.jdbc.Driver");
-//		} catch (ClassNotFoundException e) {
-//			System.out.println("Where is your MySQL JDBC Driver?");
-//			e.printStackTrace();
-//			return;
-//		}
-//	 
-//		System.out.println("MySQL JDBC Driver Registered!");
-//		Connection connection = null;
-//	 
-//		try {
-//			connection = DriverManager
-//			.getConnection("jdbc:mysql://localhost:3306/test","root", "abc123");
-//	 
-//		} catch (SQLException e) {
-//			System.out.println("Connection Failed! Check output console");
-//			e.printStackTrace();
-//			return;
-//		}
-//	 
-//		if (connection != null) {
-//			System.out.println("You made it, take control your database now!");
-//			String sql = "Select * from pet where name=?";
-//			PreparedStatement ps = connection.prepareStatement(sql);
-//			ps.setString(1, "here");
-//			ResultSet rs = ps.executeQuery();
-//			
-//			while(rs.next())
-//			{
-//				String name = rs.getString("name");
-//				
-//				System.out.println(name);
-//			}
-//		} else {
-//			System.out.println("Failed to make connection!");
-//		}
-//	  }
-	  
-	  
-	  public void createName(String name) throws SQLException
-	  {
+	public void createName(String name) throws SQLException
+	{
 		  String sql = "Insert into pet(name) values(?)";
 		  
 		  PreparedStatement ps = connection.prepareStatement(sql);
@@ -91,9 +71,8 @@ public class DatabaseHandler {
 		  ps.execute();
 		  
 		  ps.setString(1, "GOOGLE");
-		  ps.execute();
-		  
-	  }
+		  ps.execute();  
+	}
 	  
 	  public String[] getNames() throws SQLException
 	  {		 
