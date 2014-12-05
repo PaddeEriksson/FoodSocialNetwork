1a706aac7b87d4d2217944e61d303ec65cbf3058
KajDime <dkajhanidis@hotmail.com>
2014-12-05 19:29:37 +0100

    Dummy

--- WebPage/JS/User_Info.js
+++ WebPage/JS/User_Info.js
@@ -1,14 +1,6 @@
 var UserInfo = angular.module('UserInfo',['ui.bootstrap']);
-UserInfo.controller('Login',function($scope)
-{
-	$scope.setInfo = {Name:'User Name', PWord:'PassWord'};
-	$scope.Submit_c = function()
-	{
-		//send info to server!!!
-	};
 
-});
-UserInfo.controller('ModalDemoCtrl', function ($scope, $modal, $log) {
+UserInfo.controller('ModalUserI', function ($scope, $modal, $log) {
 
   $scope.items = ['Username:', 'Password:'];
 
@@ -36,7 +28,7 @@ UserInfo.controller('ModalDemoCtrl', function ($scope, $modal, $log) {
 // Please note that $modalInstance represents a modal window (instance) dependency.
 // It is not the same as the $modal service used above.
 
-UserInfo.controller('ModalInstanceCtrl', function ($scope, $modalInstance, items) {
+UserInfo.controller('ModalInstanceUserI', function ($scope, $modalInstance, items) {
 
   $scope.items = items;
   $scope.selected = {
