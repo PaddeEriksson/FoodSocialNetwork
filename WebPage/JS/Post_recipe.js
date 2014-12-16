fb484647d4fcdc03873fe297d96e931e55b26ca3
vina14 <vi_ckies@yahoo.com>
2014-12-16 17:14:30 +0100

    new updates to post recipe

--- WebPage/JS/Post_recipe.js
+++ WebPage/JS/Post_recipe.js
@@ -2,8 +2,33 @@ var Post_recipe = angular.module('UserInfo');
 
 Post_recipe.controller('PostRecipe', function ($scope, $modal, $log, $http) {
 
+  $scope.tempName = 'Units';
+  $scope.ingredientsInfo = {Ingredients:'', optional:'',amount:'', unit:''};
+  $scope.recipeInfo = {title:'', Instruction:'', Time:''};
+  $scope.unitsSgs = {
+    kg:'Kg',
+    mg:'Mg',
+    tbsp:'tbsp',
+    L:'litres',
+    ml:'ml',
+    tsp:'tsp'
+  };
 
-  $scope.items = {title:'', Instruction:'', Time:'', Ingredients:'', optional:'',amount:'', unit:'', add: '' };
+  $scope.status = {
+    isopen: false
+  };
+  $scope.ChngBtnName = function(newName){
+    $scope.tempname = newName;
+  };
+  $scope.toggled = function(open) {
+    $log.log('Dropdown is now: ', open);
+  };
+
+  $scope.toggleDropdown = function($event) {
+    $event.preventDefault();
+    $event.stopPropagation();
+    $scope.status.isopen = !$scope.status.isopen;
+  };
 
   $scope.RecepieWO = function (size) {
 
@@ -13,28 +38,33 @@ Post_recipe.controller('PostRecipe', function ($scope, $modal, $log, $http) {
       size: size,
       resolve: {
         items: function () {
-          return $scope.items;
+          return $scope.tempName;
         }
       }
     });
 
     AccountInstance.result.then(function (selectedItem) {
       $scope.selected = selectedItem;
-    }, function () {
+    },
+
+    function () {
       $log.info('Modal dismissed at: ' + new Date());
     });
-  };
+  }; 
+
 });
 // Please note that $modalInstance represents a modal window (instance) dependency.
 // It is not the same as the $modal service used above.
 
 Post_recipe.controller('Postrecipesmodal', function ($scope, $modalInstance, items, $http) {
 
-  $scope.items = items;
+
+  $scope.tempName = items;
+
  
   console.log($scope);
   $scope.selected = {
-    item: $scope.items[0]
+    item: $scope.tempName[0]
   };
 
   $scope.ok = function () {
